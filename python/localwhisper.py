cdb76176c4d13e31d109ab1c44992d380f69a7b6
66nihaal44 <nihaalshetty171@gmail.com>
2025-09-30 21:10:30 -0500

    Update localwhisper.py

--- python/localwhisper.py
+++ python/localwhisper.py
@@ -17,6 +17,6 @@ def transcribe():
     result = model.transcribe(tmp.name)
   return jsonify({"text": result["text"]})
 if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host="0.0.0.0", port=port)
+  #port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=5001)
   
