e7b4feeaac42aa1e4a2f8a9237cb66fa41af2d5f
66nihaal44 <nihaalshetty171@gmail.com>
2025-10-02 21:49:25 -0500

    Update localwhisper.py

--- python/localwhisper.py
+++ python/localwhisper.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import faster_whisper import WhisperModel 
+from faster_whisper import WhisperModel 
 import tempfile
 import os
 app = Flask(__name__)
