7392cd8c735b6c297e4a0289f7c1f795d944ee39
66nihaal44 <nihaalshetty171@gmail.com>
2025-10-05 22:10:57 -0500

    Update localwhisper.py

--- python/localwhisper.py
+++ python/localwhisper.py
@@ -13,12 +13,21 @@ def transcribe():
   if "file" not in request.files:
     return jsonify({"error": "No file uploaded"}), 400
   print("Files recieved: ", request.files);
-  file = request.files["file"]
-  with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp:
-    file.save(tmp.name)
-    segments, info = model.transcribe(tmp.name, beam_size=5, language="en")
-  text = " ".join([segment.text for segment in segments])
-  return jsonify({"text": text})
+  try:
+    file = request.files["file"]
+    if len(file.read()) > 5 * 1024 * 1024:
+      return jsonify({"error": "File too large"}), 400
+    file.seek(0);
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp:
+      file.save(tmp.name)
+      segments, info = model.transcribe(tmp.name, beam_size=5, language="en")
+    text = " ".join([segment.text for segment in segments])
+    return jsonify({"text": text})
+  except MemoryError:
+    return jsonify({"error": "Server ran out of memory."}), 500
+  except Exception as e:
+    print("Backend Error: ", e)
+    return jsonify({"error": "Unexpected backend Error"}), 500
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
   app.run(host="0.0.0.0", port=port)
