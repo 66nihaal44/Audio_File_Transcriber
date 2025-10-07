29ba9aa487b4473e6eb64ab94e5bf80202a29eaf
66nihaal44 <nihaalshetty171@gmail.com>
2025-10-06 21:53:32 -0500

    Create sentiment.py

---
+++ sentiment/sentiment.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+sentModel = pipeline("sentiment-analysis")
+def analyze_sentiment(text):
+  result = sentModel(text)[0]
+  return {
+    "label": result["label"],
+    "score": round(result["score", 3)
+  }
