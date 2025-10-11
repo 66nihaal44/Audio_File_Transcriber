63e38eb499e5823989509d80caf6109237d1d77c
66nihaal44 <nihaalshetty171@gmail.com>
2025-10-10 21:12:06 -0500

    Update transcribe.py

--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -9,11 +9,11 @@ CORS(app)# , resources={r"/*": {"origins": "https://66nihaal44.github.io"}})
 model = WhisperModel("base", device="cpu", compute_type="int8")
 @app.route("/transcribe", methods=["POST"])
 def transcribe():
-  print("Request.files.keys: ", request.files.keys())
-  print("Request.form.keys: ", request.form.keys())
+  print("Request.files.keys: ", request.files.keys(), flush=True)
+  print("Request.form.keys: ", request.form.keys(), flush=True)
   if "file" not in request.files:
     return jsonify({"error": "No file uploaded"}), 400
-  print("Files recieved: ", request.files);
+  print("Files recieved: ", request.files, flush=True);
   try:
     file = request.files["file"]
     if len(file.read()) > 5 * 1024 * 1024:
@@ -33,12 +33,12 @@ def transcribe():
     except Exception as e:
       print("Sentiment analysis failure: {e}");
       sentiment = {"error": "Sentiment analysis failed"}
-    print(text, sentiment)
+    print(text, sentiment, flush=True)
     return jsonify({"text": text, "sentiment": sentiment})
   except MemoryError:
     return jsonify({"error": "Server ran out of memory."}), 500
   except Exception as e:
-    print("Backend Error: ", e)
+    print("Backend Error: ", e, flush=True)
     return jsonify({"error": "Unexpected backend error"}), 500
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
