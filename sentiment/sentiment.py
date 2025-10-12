42d7c6acfa2a7e116317afc9dd333378ee759a09
66nihaal44 <nihaalshetty171@gmail.com>
2025-10-11 21:07:42 -0500

    Update sentiment.py

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -7,14 +7,15 @@ print("Reached sentiment.py", flush=True)
 HF_API_TOKEN = os.getenv('HF_API_TOKEN')
 @app.route("/analyze", methods=["POST"])
 def analyze_sentiment():
-  if "file" not in request.files:
-    print("No file recieved", flush=True);
-    return jsonify({"error": "No file uploaded"}), 400
-  print("Files recieved: ", request.files, flush=True);
+  data = request.get_json()
+  if not data or "text" not in data:
+    print("No json recieved", flush=True);
+    return jsonify({"error": "No json uploaded"}), 400
+  text = data["text"]
+  print("Text recieved: ", text, flush=True);
   try:
-    file = request.files["file"]
     with tempfile.NamedTemporaryFile(delete=False) as tmp:
-      file.save(tmp.name)
+      text.save(tmp.name)
       result = requests.post(
         "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english",
         headers={"Authorization": f"Bearer {HF_API_TOKEN}"},
