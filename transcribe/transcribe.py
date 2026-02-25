5d0520acd8611f6714ee07d9d54f79fe00e8580f
66nihaal44 <nihaalshetty171@gmail.com>
2026-02-24 20:25:39 -0600

    Update transcribe.py

--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -6,6 +6,7 @@ import tempfile
 import os
 app = Flask(__name__)
 CORS(app)# , resources={r"/*": {"origins": "https://66nihaal44.github.io"}})
+HF_API_TOKEN = os.getenv('HF_API_TOKEN')
 model = WhisperModel("base", device="cpu", compute_type="int8")
 @app.route("/transcribe", methods=["POST"])
 def transcribe():
