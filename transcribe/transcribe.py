2f369c2fb9bca868ab94a9e466b64f959dee3bae
66nihaal44 <nihaalshetty171@gmail.com>
2026-02-26 20:33:23 -0600

    Update CORS configuration in transcribe.py

--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -5,7 +5,7 @@ import requests
 import tempfile
 import os
 app = Flask(__name__)
-CORS(app)# , resources={r"/*": {"origins": "https://66nihaal44.github.io"}})
+CORS(app , resources={r"/*": {"origins": "https://66nihaal44.github.io"}})
 HF_API_TOKEN = os.getenv('HF_API_TOKEN')
 model = WhisperModel("base", device="cpu", compute_type="int8")
 @app.route("/transcribe", methods=["POST"])
